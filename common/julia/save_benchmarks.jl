1158d7db8ffb1410b8136deb04b4cc9533d21fb8
101001000 <kike.delacalle.montilla@gmail.com>
2024-03-11 15:08:19 +0100

    save_benchmarks refactor

--- common/julia/save_benchmarks.jl
+++ common/julia/save_benchmarks.jl
@@ -43,47 +43,27 @@ function jsons_to_csv(json_files, output_csv)
     CSV.write("csv/" * output_csv, df)
 end
 
-function generate_benchmark_csv(suffix)
-    backprop_files = ["../../julia_" * suffix * "/backprop/bpnn_layerforward_CUDA.json",
-                      "../../julia_" * suffix * "/backprop/bpnn_adjust_weights_cuda.json"]
-    jsons_to_csv(backprop_files, "backprop_" * suffix * ".csv")
-
-    bfs_files = ["../../julia_" * suffix * "/bfs/Kernel.json",
-                 "../../julia_" * suffix * "/bfs/Kernel2.json"]
-    jsons_to_csv(bfs_files, "bfs_" * suffix * ".csv")
-
-    hotspot_files = ["../../julia_" * suffix * "/hotspot/calculate_temp.json"]
-    jsons_to_csv(hotspot_files, "hotspot_" * suffix * ".csv")
-
-    leukocyte_files = ["../../julia_" * suffix * "/leukocyte/GICOV_kernel.json",
-                       "../../julia_" * suffix * "/leukocyte/dilate_kernel.json",
-                       "../../julia_" * suffix * "/leukocyte/IMGVF_kernel.json"]
-    jsons_to_csv(leukocyte_files, "leukocyte_" * suffix * ".csv")
-
-    lud_files = ["../../julia_" * suffix * "/lud/lud_diagonal.json",
-                 "../../julia_" * suffix * "/lud/lud_perimeter.json",
-                 "../../julia_" * suffix * "/lud/lud_internal.json"]
-    jsons_to_csv(lud_files, "lud_" * suffix * ".csv")
-
-    euclid_files = ["../../julia_" * suffix * "/nn/euclid.json"]
-    jsons_to_csv(euclid_files, "nn_" * suffix * ".csv")
-
-    nw_files = ["../../julia_" * suffix * "/nw/needle_cuda_shared_1.json",
-                "../../julia_" * suffix * "/nw/needle_cuda_shared_2.json"]
-    jsons_to_csv(nw_files, "nw_" * suffix * ".csv")
-
-    particle_filter_files = ["../../julia_" * suffix * "/particlefilter/likelihood_kernel.json",
-                             "../../julia_" * suffix * "/particlefilter/sum_kernel.json",
-                             "../../julia_" * suffix * "/particlefilter/normalize_weights_kernel.json",
-                             "../../julia_" * suffix * "/particlefilter/find_index_kernel.json"]
-    jsons_to_csv(particle_filter_files, "particle_filter_" * suffix * ".csv")
-
-    pathfinder_files = ["../../julia_" * suffix * "/pathfinder/dynproc_kernel.json"]
-    jsons_to_csv(pathfinder_files, "pathfinder_" * suffix * ".csv")
-
-    stream_cluster_files = ["../../julia_" * suffix * "/streamcluster/kernel_compute_cost.json"]
-    jsons_to_csv(stream_cluster_files, "stream_cluster_" * suffix * ".csv")
+function generate_benchmark_csv(benchmark, kernels, suffix)
+    kernel_files = []
+    for kernel in kernels
+        push!(kernel_files, "../../julia_" * suffix * "/" * benchmark * "/" * kernel * ".json")
+    end
+    jsons_to_csv(kernel_files, benchmark * "_" * suffix * ".csv")
 end
 
-generate_benchmark_csv("cuda")
-generate_benchmark_csv("gen")
+function generate_benchmarks_csv(suffix)
+    generate_benchmark_csv("backprop", ["bpnn_layerforward_CUDA", "bpnn_adjust_weights_cuda"], suffix)
+    generate_benchmark_csv("bfs", ["Kernel_$(i)" for i in 0:11], suffix)
+    generate_benchmark_csv("bfs", ["Kernel2_$(i)" for i in 0:11], suffix)
+    generate_benchmark_csv("hotspot", ["calculate_temp"], suffix)
+    generate_benchmark_csv("leukocyte", ["GICOV_kernel", "dilate_kernel", "IMGVF_kernel"], suffix)
+    generate_benchmark_csv("lud", ["lud_diagonal", "lud_perimeter", "lud_internal"], suffix)
+    generate_benchmark_csv("nn", ["euclid"], suffix)
+    generate_benchmark_csv("nw", ["needle_cuda_shared_1", "needle_cuda_shared_2"], suffix)
+    generate_benchmark_csv("particlefilter", ["likelihood_kernel", "sum_kernel", "normalize_weights_kernel", "find_index_kernel"], suffix)
+    generate_benchmark_csv("pathfinder", ["dynproc_kernel"], suffix)
+    generate_benchmark_csv("streamcluster", ["kernel_compute_cost"], suffix)
+end
+
+generate_benchmarks_csv("cuda")
+generate_benchmarks_csv("gen")
